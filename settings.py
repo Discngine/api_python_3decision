0e471d225d0b17df25a1026fd99ffc177c1a2894
Sheraf-DNG <36472025+Sheraf-DNG@users.noreply.github.com>
2020-03-12 13:58:09 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,17 @@
-# auth_type =   cloud | on_prem
-# x_api_secret, user, mail, verifySSL must be defined
+# auth_type authorized values =   cloud  |  on_prem
 
-# auth_type =  on_prem
+# For 'on_prem' authentication, the following properties of the params object must be defined:
 # base_url, user, mail, password, verifySSL must be defined
+
+# For 'cloud' authentication, the following properties of the params object must be defined:
+# base_url, user, mail, x_api_secret, verifySSL must be defined
 
 # base_url example: https://3decision.discngine.cloud
 # api_path_example: /api/v2
 
 params = {
     'base_url'      : '',
-    'api_path'      : '/api/v2',
+    'api_path'      : '/api/v2', 
     'auth_type'     : '',
     'x_api_secret'  : '',
     'mail'          : '',
