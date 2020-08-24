3828a5327dd44c0932da95f933ea3438031d4ff9
dalvgar <daniel.alvarez@discngine.com>
2020-08-24 12:38:47 +0000

    add Dockerfile for 3decision api

---
+++ docker/api_conf/settings.py
@@ -0,0 +1,10 @@
+params = {
+        'base_url'      : 'https://3decision.discngine.cloud',
+        'api_path'      : '/api/v2',
+        'auth_type'     : 'cloud',
+        'x_api_secret'  : 'xxxxxx.xxxx-xxxxxx-xxxx',
+        'mail'          : 'mydomain@domain.com',
+        'user'          : 'only_for_on_prem',
+        'password'      : 'only_for_on_prem',
+        'verifySSL'     : True
+    }
